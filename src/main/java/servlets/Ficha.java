96b0fdfc6c3534cca0c1a1daec704a1a72518d96
seshadevlo <l.vet.operaciones@gmail.com>
2025-10-24 21:28:21 -0300

    primera version de formulario

---
+++ src/main/java/servlets/Ficha.java
@@ -0,0 +1,68 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Ficha
+ */
+@WebServlet("/Ficha")
+public class Ficha extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Ficha() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//respuesta
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		
+		String nombre = request.getParameter("nombre");
+		String apellido = request.getParameter("apellido");
+		String lenguaje = request.getParameter("lenguaje");
+		
+		
+		out.println("<html>"+"<head> <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css\" rel=\"stylesheet\"></head>"
+		+"<body>");
+		
+		out.println("<div class='container mt-5'>");//"<div></div>"
+		out.println("<h1>Bienvenido"+" "+nombre+" "+apellido+"</h1>");
+		out.println("tu lenguaje de programacion favorito es:"+" "+lenguaje);
+		
+		out.println("</div>"+"</body>"+"</html>");
+						
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
